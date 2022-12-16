e61ed1323cf5f1ff85c1192f84352816b4cfacdc
Wilsondotzip <wspalding5@gmail.com>
2022-12-16 20:19:34 +1000

    Support for more knobs + Removed fluctuations
    -Added support for multiple knobs
    -Added averaging of values to remove fluctuations.
    -Cleaned things up

---
+++ Firmware/main/main.ino
@@ -0,0 +1,68 @@
+int pot1 = A0;
+int pot2 = A1;
+int pot3 = A2;
+int pot4 = A3;
+
+int pot1Val = 0;
+int pot2Val = 0;
+int pot3Val = 0;
+int pot4Val = 0;
+
+
+int InitialValue = 0;
+
+int valA = 0;
+int valB = 0;
+int valC = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  bindToSoftware(pot1);
+  bindToSoftware(pot2);
+  bindToSoftware(pot3);
+  bindToSoftware(pot4);
+
+}
+
+void loop() {
+pot1Val = checkForUpdate(pot1, pot1Val, 1);
+pot2Val = checkForUpdate(pot2, pot2Val, 2);
+
+
+  
+
+}
+int checkForUpdate(char pin, char pinval, int ID){
+  if(readAv(pin)== pinval)  {}
+  else{
+  Serial.print(readAv(pin));
+  Serial.print("@");
+  Serial.print(ID);
+  Serial.print("\n");
+  return readAv(pin);
+
+    }  
+  }
+  
+int readAv(char pin){
+  int i;  
+  int sval = 0;
+
+  for (i = 0; i < 5; i++){
+    sval = sval + map(analogRead(pin), 0, 1023, 100, 0);
+    delay(1);
+  }
+  sval = sval / 5; 
+
+  return sval;
+}
+  
+char bindToSoftware(char pin){
+  InitialValue = map(analogRead(pin), 0, 1023, 100, 0);
+  Serial.print(InitialValue);
+  Serial.print("@1");
+  Serial.print("\n");
+
+  }
