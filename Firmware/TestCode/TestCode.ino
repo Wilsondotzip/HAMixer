160600741b45d97a0db10f388b8aa8694604bd14
Wilsondotzip <wspalding5@gmail.com>
2022-12-16 23:06:05 +1000

    Better Testcode
    testcode now tests all IDs

---
+++ Firmware/TestCode/TestCode.ino
@@ -0,0 +1,60 @@
+int InitialValue = 0;
+int valA = 0;
+int valB = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.print(valA);
+  Serial.print("@1");
+  Serial.print("\n");
+  Serial.print(valA);
+  Serial.print("@2");
+  Serial.print("\n");
+  Serial.print(valA);
+  Serial.print("@3");
+  Serial.print("\n");
+  Serial.print(valA);
+  Serial.print("@4");
+  Serial.print("\n");
+}
+
+void loop() {
+  
+if (valB==0||valA==0){
+  valB = 0;
+  Serial.print(valA);
+  Serial.print("@1");
+  Serial.print("\n");
+  Serial.print(valA);
+  Serial.print("@2");
+  Serial.print("\n");
+  Serial.print(valA);
+  Serial.print("@3");
+  Serial.print("\n");
+  Serial.print(valA);
+  Serial.print("@4");
+  Serial.print("\n");
+  valA++;
+
+    }
+    
+if (valA==100||valB==1){
+  valB = 1;
+  Serial.print(valA);
+  Serial.print("@1");
+  Serial.print("\n");
+  Serial.print(valA);
+  Serial.print("@2");
+  Serial.print("\n");
+  Serial.print(valA);
+  Serial.print("@3");
+  Serial.print("\n");
+  Serial.print(valA);
+  Serial.print("@4");
+  Serial.print("\n");
+  valA--;  
+      
+      }
+delay(100);
+
+}
