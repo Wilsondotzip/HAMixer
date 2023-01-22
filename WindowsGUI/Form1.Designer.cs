dc6029f2626f898e2607923e142706cf60f4e526
Antony Naumovic <59066658+antonynaumovic-alt@users.noreply.github.com>
2023-01-22 10:28:49 +0000

    Windows GUI Updates

---
+++ WindowsGUI/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace HAM_Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.hamIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.loadConfigFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxBaudrate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxBytesize = new System.Windows.Forms.TextBox();
+            this.textBoxParity = new System.Windows.Forms.TextBox();
+            this.textBoxStopbits = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.idSelect = new System.Windows.Forms.NumericUpDown();
+            this.buttonSaveMap = new System.Windows.Forms.Button();
+            this.openConfigDialog = new System.Windows.Forms.OpenFileDialog();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.idSelect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hamIcon
+            // 
+            this.hamIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.hamIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("hamIcon.Icon")));
+            this.hamIcon.Text = "notifyIcon1";
+            this.hamIcon.Visible = true;
+            this.hamIcon.MouseDown += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadConfigFileToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(161, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // loadConfigFileToolStripMenuItem
+            // 
+            this.loadConfigFileToolStripMenuItem.Name = "loadConfigFileToolStripMenuItem";
+            this.loadConfigFileToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.loadConfigFileToolStripMenuItem.Text = "Load Config File";
+            this.loadConfigFileToolStripMenuItem.Click += new System.EventHandler(this.loadConfigFileToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(160, 22);
+            this.toolStripMenuItem1.Text = "Documentation";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(191, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // textBoxBaudrate
+            // 
+            this.textBoxBaudrate.Location = new System.Drawing.Point(191, 63);
+            this.textBoxBaudrate.Name = "textBoxBaudrate";
+            this.textBoxBaudrate.Size = new System.Drawing.Size(121, 23);
+            this.textBoxBaudrate.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "COM Port Selection";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Baudrate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bytesize:";
+            // 
+            // textBoxBytesize
+            // 
+            this.textBoxBytesize.Location = new System.Drawing.Point(191, 92);
+            this.textBoxBytesize.Name = "textBoxBytesize";
+            this.textBoxBytesize.Size = new System.Drawing.Size(121, 23);
+            this.textBoxBytesize.TabIndex = 8;
+            // 
+            // textBoxParity
+            // 
+            this.textBoxParity.Location = new System.Drawing.Point(191, 121);
+            this.textBoxParity.Name = "textBoxParity";
+            this.textBoxParity.Size = new System.Drawing.Size(121, 23);
+            this.textBoxParity.TabIndex = 9;
+            // 
+            // textBoxStopbits
+            // 
+            this.textBoxStopbits.Location = new System.Drawing.Point(191, 150);
+            this.textBoxStopbits.Name = "textBoxStopbits";
+            this.textBoxStopbits.Size = new System.Drawing.Size(121, 23);
+            this.textBoxStopbits.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Parity:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Stopbits:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(237, 426);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 13;
+            this.saveButton.Text = "Save All";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(70, 249);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(214, 71);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // idSelect
+            // 
+            this.idSelect.Location = new System.Drawing.Point(70, 220);
+            this.idSelect.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.idSelect.Name = "idSelect";
+            this.idSelect.Size = new System.Drawing.Size(120, 23);
+            this.idSelect.TabIndex = 15;
+            this.idSelect.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.idSelect.ValueChanged += new System.EventHandler(this.idSelect_ValueChanged);
+            // 
+            // buttonSaveMap
+            // 
+            this.buttonSaveMap.Location = new System.Drawing.Point(196, 220);
+            this.buttonSaveMap.Name = "buttonSaveMap";
+            this.buttonSaveMap.Size = new System.Drawing.Size(88, 23);
+            this.buttonSaveMap.TabIndex = 16;
+            this.buttonSaveMap.Text = "Save Map";
+            this.buttonSaveMap.UseVisualStyleBackColor = true;
+            this.buttonSaveMap.Click += new System.EventHandler(this.buttonSaveMap_Click);
+            // 
+            // openConfigDialog
+            // 
+            this.openConfigDialog.FileName = "openConfigDialog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.buttonSaveMap);
+            this.Controls.Add(this.idSelect);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxStopbits);
+            this.Controls.Add(this.textBoxParity);
+            this.Controls.Add(this.textBoxBytesize);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxBaudrate);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HAM Controller ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.idSelect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NotifyIcon hamIcon;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ComboBox comboBox1;
+        private TextBox textBoxBaudrate;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBoxBytesize;
+        private TextBox textBoxParity;
+        private TextBox textBoxStopbits;
+        private Label label4;
+        private Label label5;
+        private Button saveButton;
+        private RichTextBox richTextBox1;
+        private NumericUpDown idSelect;
+        private Button buttonSaveMap;
+        private ToolStripMenuItem loadConfigFileToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private OpenFileDialog openConfigDialog;
+    }
+}
---
+++ WindowsGUI/Form1.cs
@@ -0,0 +1,272 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Runtime.Serialization;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace HAM_Windows;
+
+public partial class Form1 : Form
+{
+    Dictionary<string, string> mapped = new();
+    Process backEndScript;
+    
+    static string defaultComport = "COM4";
+    static string defaultBaudrate = "9600";
+    static string defaultBytesize = "16";
+    static string defaultParity = "N";
+    static string defaultStopbits = "2";
+    
+    public class Config
+    {
+        [YamlMember(Alias = "comport", ApplyNamingConventions = false), DefaultValue("COM4")]
+        public string comport { get; set; }= defaultComport;
+        
+        [YamlMember(Alias = "baudrate", ApplyNamingConventions = false), DefaultValue("9600")]
+        public string baudrate { get; set; }= defaultBaudrate;
+        
+        [YamlMember(Alias = "bytesize", ApplyNamingConventions = false), DefaultValue("16")]
+        public string bytesize { get; set; }= defaultBytesize;
+        
+        [YamlMember(Alias = "parity", ApplyNamingConventions = false), DefaultValue("N")]
+        public string parity { get; set; }= defaultParity;
+        
+        [YamlMember(Alias = "stopbits", ApplyNamingConventions = false), DefaultValue("2")]
+        public string stopbits { get; set; }= defaultStopbits;
+
+
+        public Dictionary<string, AppsString>? Mappings { get; set; }
+
+        public class AppsString
+        { 
+            public string Applications { get; set; }
+        }
+    }
+
+
+    public Form1()
+    {
+        InitializeComponent();
+        //do an initial port scan
+        var ports = SerialPort.GetPortNames();
+        //backEndScript = Process.Start(@"HAM.exe");
+    }
+
+
+    private const int CP_DISABLE_CLOSE_BUTTON = 0x200;
+
+    protected override CreateParams CreateParams
+    {
+        get
+        {
+            var cp = base.CreateParams;
+            cp.ClassStyle = cp.ClassStyle | CP_DISABLE_CLOSE_BUTTON;
+            return cp;
+        }
+    }
+
+
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+        //if the form is minimized  
+        //hide it from the task bar  
+        //and show the system tray icon (represented by the NotifyIcon control)  
+        if (WindowState != FormWindowState.Minimized) return;
+        Hide();
+        hamIcon.Visible = true;
+    }
+
+
+    private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Left)
+        {
+            Show();
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+    }
+
+
+    private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+    {
+    }
+
+    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        Application.Exit();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        loadConfig(@"config.yaml");
+        BackendControl(1);
+    }
+
+    private void BackendControl(int code)
+    {
+        if (code == 1)
+            backEndScript = Process.Start(@"HAM.exe");
+        else
+            foreach (var process in Process.GetProcessesByName("HAM"))
+                process.Kill();
+    }
+
+    private void comboBox1_Click(object sender, EventArgs e)
+    {
+        // Display each port name to the console.
+        comboBox1.Items.Clear();
+
+        comboBox1.Items.AddRange(SerialPort.GetPortNames());
+    }
+
+    private void saveButton_Click(object sender, EventArgs e)
+    {
+        saveConfig();
+        MessageBox.Show("config.yaml has been saved");
+    }
+
+    private string? tryGetID(string key)
+    {
+        return mapped.TryGetValue(key, out string? value) ? value : "";
+    }
+
+    private void loadConfig(string path)
+    {
+        if (!File.Exists(path))
+        {
+            var newFile = File.Create(path);
+            newFile.Close();
+        }
+        string text = File.ReadAllText(path); //read file
+        richTextBox1.Text = text; //quick debug flash to know the file is being read
+
+        //deserialize file
+        var deserializer = new DeserializerBuilder().IgnoreUnmatchedProperties().Build();
+        Config deserialized = deserializer.Deserialize<Config>(text);
+
+        if (deserialized == null)
+        {
+            deserialized = new Config();
+        }
+
+        //cleanly re map do a fresh dictionary
+        mapped.Clear();
+        if (deserialized.Mappings != null)
+        {
+            foreach (var (key, value) in deserialized.Mappings)
+            {
+                mapped.Add(key, value.Applications);
+
+            }
+        }
+        else
+        {
+            mapped.Add("ID1", "");
+        }
+
+        //set the text box to a current
+        try
+        {
+            richTextBox1.Text = mapped[$"ID{idSelect.Value}"];
+        }
+        catch
+        {
+            richTextBox1.Text = "";
+
+        }
+        //set input boxes to data that was saved in config
+        comboBox1.Text = deserialized.comport != null ? deserialized.comport : defaultComport;
+        textBoxBaudrate.Text = deserialized.baudrate != null ? deserialized.baudrate : defaultBaudrate;
+        textBoxBytesize.Text = deserialized.bytesize != null ? deserialized.bytesize : defaultBytesize;
+        textBoxParity.Text = deserialized.parity != null ? deserialized.parity : defaultParity;
+        textBoxStopbits.Text = deserialized.stopbits != null ? deserialized.stopbits : defaultStopbits;
+
+    }
+
+    private void saveConfig()
+    {
+        BackendControl(0);
+        SaveCurrentMap();
+
+        var comport = comboBox1.Text;
+        var baudrate = textBoxBaudrate.Text;
+        var parity = textBoxParity.Text;
+        var stopbits = textBoxStopbits.Text;
+        var bytesize = textBoxBytesize.Text;
+
+        var mappings = mapped.Aggregate("", (current, map) => current + $"    {map.Key}:\n        Applications: {map.Value}\n");
+        
+        Console.WriteLine(mappings);
+        //config gen
+        var yml = @$"
+comport: {comport}
+baudrate: {baudrate}
+bytesize: {bytesize}
+parity: {parity}
+stopbits: {stopbits}
+
+Mappings:
+{mappings}
+
+";
+        
+        Console.Write(yml);
+        File.WriteAllText("config.yaml", yml);
+
+        loadConfig(@"config.yaml");
+        BackendControl(1);
+    }
+
+    private void idSelect_ValueChanged(object sender, EventArgs e)
+    {
+        richTextBox1.Text = mapped.TryGetValue($"ID{idSelect.Value}", out var stored) ? stored : "";
+    }
+
+    private void buttonSaveMap_Click(object sender, EventArgs e)
+    {
+        SaveCurrentMap();
+    }
+
+    private void SaveCurrentMap()
+    {
+        if (mapped.ContainsKey($"ID{idSelect.Value}"))
+        {
+            if (mapped[$"ID{idSelect.Value}"] == richTextBox1.Text) return;
+            mapped.Remove($"ID{idSelect.Value}");
+            mapped.Add($"ID{idSelect.Value}", richTextBox1.Text);
+        }
+        else
+        {
+            mapped.Add($"ID{idSelect.Value}", richTextBox1.Text);
+        }
+    }
+    
+    private void richTextBox1_TextChanged(object sender, EventArgs e)
+    {
+    }
+
+    private void toolStripMenuItem1_Click(object sender, EventArgs e)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "https://github.com/Wilsondotzip/HardwareApplicationMixer",
+            UseShellExecute = true
+        });
+    }
+
+    private void loadConfigFileToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        if (openConfigDialog.ShowDialog() != DialogResult.OK) return;
+        var configPath = openConfigDialog.FileName;
+        MessageBox.Show("Loading config file from:  " + configPath);
+        loadConfig(configPath);
+        saveConfig();
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        BackendControl(0);
+    }
+}
